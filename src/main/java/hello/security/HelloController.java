7ee7dc6505e2622bc3f264533348ca2123372705
Robert Sjödahl <robert@mojang.com>
2019-01-31 16:37:44 +0100

    Small changes to visualize another problem with swager generation in micronaut 1.0.4

--- src/main/java/hello/security/HelloController.java
+++ src/main/java/hello/security/HelloController.java
@@ -1,22 +1,21 @@
 package hello.security;
 
-import io.micronaut.http.MediaType;
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Post;
-import io.micronaut.http.annotation.Produces;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.authentication.Authentication;
 import io.micronaut.security.rules.SecurityRule;
+import io.reactivex.Single;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 
 @Secured(SecurityRule.IS_AUTHENTICATED)
 @Tag(name = "/hello")
-@Controller("/")
+@Controller("/hello")
 public class HelloController {
-    @Produces(MediaType.TEXT_PLAIN)
-    @Post("/authenticated")
-    public String authenticated(Authentication authentication, GameReference gameReference) {
-        return authentication.getName() + " is authenticated with game reference: " + gameReference;
+    @Post("/authenticated/{helloId}")
+    public Single<? extends HttpResponse> authenticated(Authentication authentication, String helloId) {
+        return Single.just(HttpResponse.accepted());
     }
 }
